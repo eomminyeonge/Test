7313351d5dbaa8e88feb0cd8225babb54eb71b61
kosta <kosta@192.168.0.177>
2016-11-21 11:16:03 +0900

    Student class 멤버변수 추가

--- Test/src/model/Student.java
+++ Test/src/model/Student.java
@@ -1,5 +1,15 @@
 package model;
 
 public class Student extends Person{
+	private String nickname;
 
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(String name, int age, String address) {
+		super(name, age, address);
+		// TODO Auto-generated constructor stub
+	}
 }
